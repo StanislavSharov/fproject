a1e5f7a2353d310ae85b2d57739e8598b1ef9775
Stas <Sharon.stanislav19@gmail.com>
2024-09-18 20:50:48 +0200

    fcommit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+ 
+// const cat = 'toys';
+// console.log(`http://localhost${cat}`);
+// let username = 'Stash';
+// alert(`Hello ${username}`);
+// const res = confirm("Are you here?");
+// console.log(res);
+// const arr = {
+// q: 1,
+// 1: 2,
+// 2: 3,
+// abc: {
+//     bf: [{},{}],
+// def: {
+
+// }
+// }
+// };
+
+// // const array = ['a', 'b', 'c', 'd', 'e', 'f'];
+// // array[10] = '1234';
+// // console.log(array);
+// // const b = 'b';
+// // arr[b] = "1234";
+// // console.log(arr[b]);
+// // console.log(arr.q);
