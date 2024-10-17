0f69e6146ea342da257f1a404914edcfbb223202
Stas <Sharon.stanislav19@gmail.com>
2024-10-18 00:45:10 +0200

    sixecomit

--- js/script.js
+++ js/script.js
@@ -1,27 +1,17 @@
 "use strict";
+ const numberOfFilms = +prompt('How many films have you watched','');
  
-// const cat = 'toys';
-// console.log(`http://localhost${cat}`);
-// let username = 'Stash';
-// alert(`Hello ${username}`);
-// const res = confirm("Are you here?");
-// console.log(res);
-// const arr = {
-// q: 1,
-// 1: 2,
-// 2: 3,
-// abc: {
-//     bf: [{},{}],
-// def: {
-
-// }
-// }
-// };
-
-// // const array = ['a', 'b', 'c', 'd', 'e', 'f'];
-// // array[10] = '1234';
-// // console.log(array);
-// // const b = 'b';
-// // arr[b] = "1234";
-// // console.log(arr[b]);
-// // console.log(arr.q);
+ const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+ };
+        const a = prompt('Який був останній?','');
+        const b = prompt('На скільки оціниш?','');
+        const c = prompt('Який був останній?','');
+        const d = prompt('На скільки оціниш?','');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
